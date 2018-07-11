5cc47c92be8e651111512b3ec8735e415b709380
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-07-11 19:41:08 +0200

    Add: Support for groups

--- flask-docker-registry-notifications/app.py
+++ flask-docker-registry-notifications/app.py
@@ -1,13 +1,34 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
+import os
+import json
+from collections import defaultdict
+
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__)
 
+FILES = 'files'
+
+def remove_extension(filename):
+    return '.'.join(filename.split('.')[:-1])
+
+def get_groups():
+    groups = defaultdict(list)
+    files = [remove_extension(x).split('_') for x in os.listdir(FILES) if x.endswith('.json')]
+    for f in files:
+        groups[tuple(f[:3])].append(f[3])
+    return  [ {'action': g[0], 'repository': g[1], 'tag': g[2], 'items': groups[g] } for g in groups.keys()]
+
 @app.route('/<path:path>')
 def send_js(path):
-    return send_from_directory('files', path)
+    return send_from_directory(FILES, path)
+
+@app.route('/groups', methods = ['GET'])
+def groups():
+    return jsonify(get_groups())
 
 if __name__ == "__main__":
+    get_groups()
     app.run(host='0.0.0.0', port=5000)
 
 
