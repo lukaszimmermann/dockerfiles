d5ed29feedaa5a8e404c5400d7fc1813ab4812ef
Lukas Zimmermann <luk.zim91@gmail.com>
2019-11-15 22:00:54 +0100

    Some fixes

--- archlinux/repo-aur/copy_pkgs.sh
+++ archlinux/repo-aur/copy_pkgs.sh
@@ -5,6 +5,6 @@ TARGET="$1"
 mkdir -p "${TARGET}"
 find "/home/pkg/.cache/pikaur/pkg" -iname '*pkg.tar.xz' -type f -exec mv {} "${TARGET}" \;
 cd "${TARGET}"
-find . -iname '*pkg.tar.xz' -type f -print0 | xargs -0 repo -v aur.db.tar.gz
+find . -iname '*pkg.tar.xz' -type f -print0 | xargs -0 repo-add -v aur.db.tar.gz
 sync
 
