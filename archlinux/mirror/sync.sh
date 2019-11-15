96f249e472292800250925ba51a0038e81fde825
Lukas Zimmermann <luk.zim91@gmail.com>
2019-11-15 19:19:23 +0100

    Rename: arch-linux to archlinux to be consistent with Docker Hub

--- archlinux/mirror/sync.sh
+++ archlinux/mirror/sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # The script to sync a local mirror of the Arch Linux repositories and ISOs
 #
@@ -9,34 +9,46 @@
 # Licensed under the GNU GPL (version 2)
 set -euo pipefail
 
+cmd () {
+  rsync -rtLpv --delete-after --delay-updates --safe-links "${1}" "${2}" || true
+}
+
+msg () {
+  echo ">> ${1}"
+}
+
 SYNC_HOME="$1"
 SYNC_LOGS="${SYNC_HOME}/logs"
 SYNC_FILES="${SYNC_HOME}/files"
 
+msg "Sync target is: ${SYNC_HOME}"
+
 # Set repositories to sync
 SYNC_REPO=(core extra community)
 
+
 SYNC_SERVER=rsync://ftp.halifax.rwth-aachen.de/archlinux/
+
+msg "Sync server: ${SYNC_SERVER}"
 
 mkdir -p "${SYNC_HOME}"
 cd "${SYNC_HOME}"
 mkdir -p files logs scripts
 cd /tmp
 
-alias rsync2="rsync -rtLpv --delete-after --delay-updates --safe-links"
-
 if [ -z $SYNC_REPO ]; then
   # Sync a complete mirror
-  rsync2 "${SYNC_SERVER}" "${SYNC_FILES}"
+  msg "Sync all repositories"
+  cmd "${SYNC_SERVER}" "${SYNC_FILES}"
 else
+  msg "Syncing these repos: ${SYNC_REPO}"
   # Sync repositories of $SYNC_REPO
   for repo in ${SYNC_REPO[@]}; do
     repo="$(echo ${repo} | tr [:upper:] [:lower:])"
     echo ">> Syncing ${repo} to ${SYNC_FILES}/${repo}"
-    rsync2 "${SYNC_SERVER}/${repo}" "${SYNC_FILES}"
+    cmd "${SYNC_SERVER}/${repo}" "${SYNC_FILES}"
     sleep 5 
   done
 fi
-unalias rsync2
 sync
 
