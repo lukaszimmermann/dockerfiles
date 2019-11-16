944ec16083ac85e095c02940d511e0c7a0e5421d
Lukas Zimmermann <luk.zim91@gmail.com>
2019-11-16 22:18:44 +0100

    fix

--- archlinux/repo-aur/copy_pkgs.sh
+++ archlinux/repo-aur/copy_pkgs.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 TARGET="$1"
+
+rm -rf "${TARGET}"
 mkdir -p "${TARGET}"
+
 find "/home/pkg/.cache/pikaur/pkg" -iname '*pkg.tar.xz' -type f -exec mv {} "${TARGET}" \;
 cd "${TARGET}"
 find . -iname '*pkg.tar.xz' -type f -print0 | xargs -0 repo-add -v aur.db.tar.gz
